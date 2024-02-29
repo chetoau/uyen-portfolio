951733bf54151bd0ccccab9d1a622e174cd25b69
Nhu Vu <geniuslab00@gmail.com>
2024-02-28 20:06:08 -0800

    changed homepage

--- src/components/Play.js
+++ src/components/Play.js
@@ -4,9 +4,11 @@ import SFMoma from '../images/sfmoma-cover.jpg';
 import SniffSniff from '../images/sniffsniff-cover.jpg';
 import { Helmet } from "react-helmet";
 import Project from './Project';
-import OpenProject from '../images/openproject-cover.jpg';
+import flower from '../images/flower-cover.jpg';
 import Soler from '../images/soler-cover.jpg';
 import InnovativeDesign from '../images/innovative-design-cover.jpg';
+import peets from '../images/peets-cover.jpg';
+
 
 export default function Play() {
     return (
@@ -20,7 +22,8 @@ export default function Play() {
                     <p className={styles.title}>LET'S HAVE FUN!</p>
                     <p className={styles.subtitle}>Designing is my gateway to boundless creativity and joy.<br/>Explore some of my exciting side projects!</p>
                     <div className={styles.playprojects}>
-                        <Project img={OpenProject} name="Open Project" subtitle="Club's Website" keywords="WEBSITE DESIGN / BRANDING" description="Open Project is a UC Berkeley club redefining the culture of tech clubs at UC Berkeley embodying values of inclusivity, passion, and innovation." url="/open-project" />
+                        <Project img={flower} name="Flower" subtitle="Motivating Podcast" keywords="BRAND IDENTITY / ILLUSTRATIONS" description="Flower Podcast ignites passion, creativity, positivity, and growth through various audio options." url="/flower" />
+                        <Project img={peets} name="Peet's Coffee" subtitle="Fall Campaign" keywords="MARKETING / VISUAL DESIGN" description="Marketing Peet's annual Fall campaign through various mediums: prints, poster designs, packaging, employee's merch, website design and ads." url="/peets" />
                         <Project img={Soler} name="Soler" subtitle="AI Generated Brand" keywords="BRANDING / HACKATHON / AI" description="Soler, a Playbook Hackathon creation, is an innovative AI generated shoe brand that earned us first place and a cash prize of $1000!" url="/soler" />
                         <Project img={InnovativeDesign} name="Innovative Design" subtitle="VP of Marketing" keywords="MARKETING / VISUAL DESIGN" description="Innovative Design is a UC Berkeley's pro-bono creative agency. Marketing serves as the backbone of the club ensuring our events, club info, and mission spreads throughout campus!" url="/innovative-design" />
                         <Project img={SFMoma} name="San Francisco Museum of Modern Art" subtitle="Halloween Poster" keywords="POSTER DESIGN / DESIGNATHON" description="A poster for SF MoMA Halloween Block Party, celebrating the resilient community post-pandemic." url="/sf-moma" />
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,20 +2,18 @@ import Project from './Project';
 import Sticker from './Sticker';
 import Contact from './Contact';
 import styles from '../css/Projects.module.css';
-import playpeople from '../images/playpeople.png';
-import justworks from '../images/justworks-cover.jpg';
-import communitygearbox from '../images/community-gearbox-cover.jpg';
-import re from '../images/re-cover.jpg';
-import peets from '../images/peets-cover.jpg';
-import flower from '../images/flower-cover.jpg';
+import justworks from '../images/justworks-cover.png';
+import communitygearbox from '../images/communitygearbox-cover.png';
+import re from '../images/RE_cover.png';
 import brave from '../images/brave-sticker.png';
 import bloom from '../images/bloom-sticker.png';
 import kind from '../images/kind-sticker.png';
 import breathe from '../images/breathe-sticker.png';
-import arrow from '../images/arrow.png';
-import playperson from '../images/playperson.png';
 import { Helmet } from "react-helmet";
-import pinterest from '../images/pinterest.jpeg'
+import pinterest from '../images/pinterest-cover.png'
+import flowerSticky from '../images/orange-flower.png'
+import starSticky from '../images/orange-star.png'
+import underline from '../images/underline.png'
 
 export default function Projects() {
     return (
@@ -26,17 +24,28 @@ export default function Projects() {
             </Helmet>
             <div className="limitWidth">
                 <div className={styles.frontline}>
-                    <div className={styles.relativeWrapper}>
-                        <p className={styles.translation}>hello in viet!</p>
-                        <p className={styles.helloSticky}>XIN CH&Agrave;O</p>
-                        <h1>COME ON INTO<br/>UYEN'S WORLD</h1>
-                        <img className={styles.arrow} src={arrow} alt=""/>
+                    <div className={styles.left}>
+                        <div className={styles.relativeWrapper}>
+                            <p className={styles.bigFont}>I'm Uyen ᵕ̈</p>
+                            <p className={styles.helloSticky}>xin ch&agrave;o</p>
+                            <h1>CURIOUS</h1>
+                            <img src={flowerSticky} className={styles.flowerSticky}/>
+                            <h1>IMPACTFUL</h1>
+                            <img src={underline} className={styles.underLined}/>
+                            <h1>EMPATHETIC</h1>
+                            <div className={styles.starSubsection}>
+                                <img src={starSticky} className={styles.starSticky}/>
+                                <p className={styles.bigFont}>Product Designer</p>
+                            </div>
+                        </div>
                     </div>
-                    <p className={styles.frontlineSubContent}>Uyen is a product designer aiming to weave together simplicity, joy, and connection through human-centric thinking</p>
-                    <div className={styles.relativeWrapper}>
-                        <img className={styles.playpeople} src={playpeople} alt=""/>
-                        <img className={styles.playperson} src={playperson} alt=""/>
-                        <p className={styles.descriptionSticky}>DESIGNING PRODUCTS WITH EMPATHY AS THE CORE</p>
+                    <div className={styles.right}>
+                        <p>Uyen is a <span className={styles.orangeBold}>product designer</span> who aims to weave together simplicity, joy, and connection through human-centric thinking.</p>
+                        <ul>
+                            <li>Studies Cognitive Science & Data Science <span className={styles.bold}>@ UC Berkeley</span></li>
+                            <li>Previously at <span className={styles.bold}>Justworks & Pinterest</span></li>
+                            <li>Currently a project mentor at <span className={styles.bold}>Unilever</span> through Berkeley Innovation </li>
+                        </ul>
                     </div>
                 </div>
                 <div className={styles.projects}>
@@ -44,8 +53,7 @@ export default function Projects() {
                     <Project img={justworks} name="Justworks" subtitle="Mobile Directory" keywords="UX RESEARCH / MOBILE DESIGN / UI" description="Developed a mobile directory, streamlining access to important  information for over 1000 companies and their employees." url="/justworks" />
                     <Project img={communitygearbox} name="Community Gearbox" subtitle="Community Building" keywords="PRODUCT DESIGN /  PROTOTYPING" description="Creating mobile features to increase accountability and foster community engagement." url="/community-gearbox" />
                     <Project img={re} name="RE:" subtitle="Bridging LDRs" keywords="DESIGN METHODOLOGY / UX / UI" description="RE: is a prompts + recording app designed to foster connection between two individuals, transcending physical barriers." url="/re" />
-                    <Project img={peets} name="Peet's Coffee" subtitle="Fall Campaign" keywords="MARKETING / VISUAL DESIGN" description="Marketing Peet's annual Fall campaign through various mediums: prints, poster designs, packaging, employee's merch, website design and ads." url="/peets" />
-                    <Project img={flower} name="Flower" subtitle="Motivating Podcast" keywords="BRAND IDENTITY / ILLUSTRATIONS" description="Flower Podcast ignites passion, creativity, positivity, and growth through various audio options." url="/flower" />
+
                 </div>
                 <div className={styles.stickers}>
                     <p className={styles.surprise}>A SURPRISE?</p>
